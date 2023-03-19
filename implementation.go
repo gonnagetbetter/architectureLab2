1372d6108965c9738de87fb3e739b0bfb67e6a73
den <denissytnikmacho@gmail.com>
2023-03-19 17:21:45 +0200

    implemented implementation

--- implementation.go
+++ implementation.go
@@ -1,9 +1,35 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func PostfixToPrefix(postfix string) (string, error) {
+    stack := make([]string, 0)
+    operators := map[string]bool{"+": true, "-": true, "*": true, "/": true, "^": true}
+
+    if postfix == "" {
+        return "", fmt.Errorf("invalid expression")
+    }
+
+    for _, token := range strings.Split(postfix, " ") {
+        if _, isOperator := operators[token]; isOperator {
+            if len(stack) < 2 {
+                return "", fmt.Errorf("not enough operands")
+            }
+            operand1, operand2 := stack[len(stack)-1], stack[len(stack)-2]
+            stack = stack[:len(stack)-2]
+            prefix := fmt.Sprintf("%s %s %s", token, operand2, operand1)
+            stack = append(stack, prefix)
+        } else {
+            stack = append(stack, token)
+        }
+    }
+
+    if len(stack) != 1 {
+        return "", fmt.Errorf("invalid expression")
+    }
+
+    return stack[0], nil
 }
