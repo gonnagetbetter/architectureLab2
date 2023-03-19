67de9d939820809b887ea9d130e23ec4d39e2eed
Alzex <alexzemly@gmail.com>
2023-03-19 17:23:40 +0200

    Added tests

--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,49 @@
 package lab2
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
+func TestPostfixToPrefix1(t *testing.T) {
+	res, err := PostfixToPrefix("1 2 +")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, "+ 1 2", res)
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+func TestPostfixToPrefix2(t *testing.T) {
+	res, err := PostfixToPrefix("1 2 + 3 ^")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "^ + 1 2 3", res)
+	}
+}
 
-	// Output:
-	// 2 2 +
+func TestPostfixToPrefix3(t *testing.T) {
+	res, err := PostfixToPrefix("1 2 3 + * 4 ^")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "^ * 1 + 2 3 4", res)
+	}
+}
+
+func TestPostfixToPrefix4(t *testing.T) {
+	res, err := PostfixToPrefix("1 2 - 3 4 + /")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "/ - 1 2 + 3 4", res)
+	}
+}
+
+func TestPostfixToPrefix5(t *testing.T) {
+	res, err := PostfixToPrefix("1 2 3 + * 4 - 5 / 6 7 + *")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "* / - * 1 + 2 3 4 5 + 6 7", res)
+	}
+}
+
+func TestPostfixToPrefix6(t *testing.T) {
+	res, err := PostfixToPrefix("1 2 * 3 4 - / 5 6 + + 7 8 9 - - 10 - *")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "* + / * 1 2 - 3 4 + 5 6 - - 7 - 8 9 10", res)
+	}
 }
