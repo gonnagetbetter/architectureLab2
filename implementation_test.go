10564d84db1b9872d514ce446f00b3ae89ed8885
den <denissytnikmacho@gmail.com>
2023-03-19 17:24:57 +0200

    created tests for errors

--- implementation_test.go
+++ implementation_test.go
@@ -1,6 +1,7 @@
 package lab2
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,5 +46,34 @@ func TestPostfixToPrefix6(t *testing.T) {
 	res, err := PostfixToPrefix("1 2 * 3 4 - / 5 6 + + 7 8 9 - - 10 - *")
 	if assert.Nil(t, err) {
 		assert.Equal(t, "* + / * 1 2 - 3 4 + 5 6 - - 7 - 8 9 10", res)
+	}
+}
+
+// Testing errors
+func TestPostfixToPrefixEmptyString(t *testing.T) {
+	_, err := PostfixToPrefix("")
+	if assert.Error(t, err) {
+			assert.Equal(t, errors.New("invalid expression"), err)
+	}
+}
+
+func TestPostfixToPrefixInvalidString1(t *testing.T) {
+	_, err := PostfixToPrefix("ussr anthem bass boosted")
+	if assert.Error(t, err) {
+			assert.Equal(t, errors.New("invalid expression"), err)
+	}
+}
+
+func TestPostfixToPrefixInvalidString2(t *testing.T) {
+	_, err := PostfixToPrefix("1 2 + clown * 3 / 4")
+	if assert.Error(t, err) {
+			assert.Equal(t, errors.New("invalid expression"), err)
+	}
+}
+
+func TestPostfixToPrefixInvalidString3(t *testing.T) {
+	_, err := PostfixToPrefix("1 2 * -")
+	if assert.Error(t, err) {
+			assert.Equal(t, errors.New("not enough operands"), err)
 	}
 }
